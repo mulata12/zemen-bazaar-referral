227cfc160963522ad6e670339466757acdcdf0d7
Mulata Niguse <mulatanig27@gmail.com>
2026-08-08 18:14:28 +0300

    Update next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  typescript: {
+    // Allows production builds to successfully complete even if your project has type errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allows production builds to successfully complete even if your project has ESLint errors
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     // Remove or comment out the old option
     // allowedDevOrigins: ['http://localhost:3000'], 
